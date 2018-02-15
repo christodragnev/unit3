d996272169f5ea9f188a90f35ebc8babee5afdae
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-02-15 08:45:15 -0500

    Updated from Brython Server: 2/15/2018 8:45:15 AM

--- gauss.py
+++ gauss.py
@@ -1,6 +1,20 @@
 #Christo Dragnev
 #2/15/18
 #gauss.py
-
+""" - adding numbers between 1,100
+total = 0
 for i in range(1,101):
-    print()
+    total+=i
+print(total)
+"""
+
+""" - inputing numbers 
+num1 = int(input('Enter a number instead of 1: '))
+num2 = int(input('Enter a number instead of 100: '))
+
+total = 0
+for i in range(num1, num2+1):
+    total+=i
+print(total)
+"""
+
