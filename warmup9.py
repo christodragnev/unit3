fefc7d56c3182ba1ada38693c599d95e6c1c5bd4
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-01 08:35:26 -0500

    Update warmup9.py

--- warmup9.py
+++ warmup9.py
@@ -5,7 +5,7 @@
 text = input('Enter text: ')
 
 for ch in text:
-    if 'a' in ch or 'e' in ch or 'i' in ch or 'o' in ch or 'u' in ch or 'y' in ch:
+    if ch in 'aeiouAEIOU':
         print(ch.upper())
     else:
         print(ch.lower())
