12d561df3a1632ff3e0e1398d08dbf2c16ac4a0b
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-01 08:30:50 -0500

    Updated from Brython Server: 3/1/2018 8:30:49 AM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,11 @@
+#Christo Dragnev
+#3/1/18
+#warmup9.py
 
+text = input('Enter text: ')
+
+for ch in text:
+    if 'a' in ch or 'e' in ch or 'i' in ch or 'o' in ch or 'u' in ch or 'y' in ch:
+        print(ch.upper())
+    else:
+        print(ch.lower())
