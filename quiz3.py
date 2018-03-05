aabb1dc71426ea77a6ef30bac2c4e142de9e2ea5
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-05 08:38:31 -0500

    Updated from Brython Server: 3/5/2018 8:38:31 AM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,23 @@
+#Christo Dragnev
+#3/5/18
+#quiz3.py
+
+
+i=-16
+while i<=-10:
+    i+=1
+    print(i)
+
+for i in range(18,51,4):
+    print(i)
+
+total = 0
+for i in range(-100,1001,2):
+    total+=i
+print(total)
+
+while True:
+    text = input('Enter text: ')
+    if 'alpaca' in text:
+        break
 
